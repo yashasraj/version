f83bc2fb6c9f24edf4142434096fe4a37fb11e13
yashasraj <yashasraj15@gmail.com>
2022-10-07 18:15:57 +0530

    updated

--- yash.ps1
+++ yash.ps1
@@ -1,6 +1,7 @@
-New-Item "C:\AWSS" -itemType Directory
-Set-Location -Path C:\AWSS\
-new-item output.txt
+New-Item "$pwd\yasss" -itemType Directory
+Set-Location -Path $pwd\yasss
+new-item $pwd\output.txt
+date >> output.txt
 get-Executionpolicy -list >> output.txt
 get-wmiobject win32_product | select-object name ,version >> output.txt
 Write-Host "Congratulations! Your first script executed successfully"
